e08da84d97925a456cda8471ce5f828f7aa9076f
utsavjnn <utsavjnn@users.noreply.github.com>
2022-06-01 20:10:44 +0530

    test: test in hex wip
    Signed-off-by: utsavjnn <utsavjnn@users.noreply.github.com>

--- test/aes256keyexpansion.test.js
+++ test/aes256keyexpansion.test.js
@@ -8,7 +8,14 @@ describe("AES256 Key Expansion test", () => {
     it("Show do key expansion correctly", async() => {
         const cir = await wasmTester(path.join(__dirname,"circuits","aes256keyexpansion2.circom"));
         let ip = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
-        const witness = await cir.calculateWitness({key: ip}, true);
+        let witness = await cir.calculateWitness({key: ip});
+        witness = witness.slice(1,61);    
+        let hex_key = "";
+        for(let i=0 ;i<60; i++){
+            // console.log("xx", witness[i].toString(16));
+            hex_key+= witness[i].toString(16);
+        }
+        // let x= 99;
         console.log("witness", witness);
         assert.ok(1)
     })
