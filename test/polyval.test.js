3ca3802356dcee93834ea97a2dc1cd7b56bdc7f1
Parth Mall <parthmall0764@gmail.com>
2022-06-14 19:38:14 +0530

    fix: gfmul test and polyval test

--- test/polyval.test.js
+++ test/polyval.test.js
@@ -1,15 +1,34 @@
 const path = require("path");
 const assert = require('assert');
 const wasmTester = require('circom_tester').wasm;
+const Module = require('./module.js');
 
 
 describe("Polyval test", () => {
     it("Show do polyval hashing correctly", async() => {
         const cir = await wasmTester(path.join(__dirname,"circuits","polyval_test.circom"));
-        const input = [1,0,2,0,3,0,4,0];
+        const input = [BigInt("1"),BigInt("0"),BigInt("2"),BigInt("0"),BigInt("3"),BigInt("0"),BigInt("4"),BigInt("0")];
         const H = [BigInt("4876465869548802997"),BigInt("9173032812820655379")];
         const T = [BigInt("0"),BigInt("0")];
-        const result = [BigInt("13221735031628986036"),BigInt("7104869591649800966")];
+        // const result = [BigInt("13221735031628986036"),BigInt("7104869591649800966")];
+        var result = [];
+        Module['onRuntimeInitialized'] = function() {
+            const input_ptr = Module._malloc(input.length * BigUint64Array.BYTES_PER_ELEMENT);
+            const H_ptr = Module._malloc(H.length * BigUint64Array.BYTES_PER_ELEMENT);
+            const result_ptr = Module._malloc(T.length * BigUint64Array.BYTES_PER_ELEMENT);
+
+            Module.HEAPU64.set(new BigUint64Array(input), input_ptr/BigUint64Array.BYTES_PER_ELEMENT);
+            Module.HEAPU64.set(new BigUint64Array(H), H_ptr/BigUint64Array.BYTES_PER_ELEMENT);
+            Module.HEAPU64.set(new BigUint64Array(T), result_ptr/BigUint64Array.BYTES_PER_ELEMENT);
+
+            Module._POLYVAL(input_ptr, H_ptr, BigInt(input.length * BigInt64Array.BYTES_PER_ELEMENT), result_ptr);
+
+            for(let i=0; i<2; i++)
+            {
+                result.push(BigInt(Module.HEAPU64[result_ptr/BigUint64Array.BYTES_PER_ELEMENT + i]));
+            }
+        }
+        Module['onRuntimeInitialized']();
         let witness = await cir.calculateWitness({"in": input, "H": H, "T": T});
         witness = witness.slice(1, 3);
         console.log("Expected", result);
