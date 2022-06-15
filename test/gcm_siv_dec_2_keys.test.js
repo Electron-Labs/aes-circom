c4e0e782e73090d60d873dfcf27897bbd877b879
Parth Mall <parthmall0764@gmail.com>
2022-06-15 17:27:04 +0530

    fix: added output of success in test

--- test/gcm_siv_dec_2_keys.test.js
+++ test/gcm_siv_dec_2_keys.test.js
@@ -12,9 +12,11 @@ describe("Complete Decryption test", () => {
         const CIPHERTEXT = [133,160,27,99,2,91,161,155,127,211,221,252,3,59,62,118,201,234,198,250,112,9,66,112,46,144,134,35,131,198,195,102];
         const MSG = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
         let witness = await cir.calculateWitness({"K1": K1, "N":N, "AAD":AAD, "CIPHERTEXT":CIPHERTEXT});
+        let success = witness[17];
         witness = witness.slice(1, 17);
         console.log("Expected", MSG);
         console.log("witness", witness);
+        assert.ok(success==1);
         assert.ok(MSG.every((v, i)=> v == witness[i]));
     })
 })
